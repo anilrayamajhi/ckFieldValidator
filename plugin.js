90e71e5e79261a64510fbd9e9b6789b9d680c0c1
~A <arayamajhi@endertech.com>
2019-07-30 13:34:32 -0700

    ck field validator plugin rc

---
+++ plugin.js
@@ -0,0 +1,22 @@
+﻿
+
+/* global CKEDITOR */
+(function() {
+	'use strict';
+
+	CKEDITOR.plugins.add( 'ckFieldValidator', {
+		init : function( editor ) {
+
+			editor.on( 'required', function( evt ) {
+				var $container = $(evt.editor.container.$);
+
+				if(!$container.siblings('label.control-label').find('small').length) {
+					$container.siblings('label.control-label').append('&nbsp;&nbsp;&nbsp;&nbsp;<small style="color:#FF0000;">Required Field</small>')
+				}
+				evt.cancel();
+			} );
+
+		}
+	} );
+
+})();
